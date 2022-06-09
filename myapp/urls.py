eaac6065d92e9dafd7741f67bb86a3702002051a
Jiaqi Shao <jiaqishao5@gmail.com>
2022-06-09 15:42:34 +0100

    Set up homepage with template website (edit the html into our site)

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import re_path as url
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^$', views.homepage),
 ]
