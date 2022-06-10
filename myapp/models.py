297b84e83006ad70c3c329f1ec4cbf7a85a9b9ce
Alexmanix1 <alexandru.buruiana20@imperial.ac.uk>
2022-06-10 01:24:22 +0100

    feat: added a quiz to the homepage for milestone 1

--- myapp/models.py
+++ myapp/models.py
@@ -3,3 +3,14 @@ from django.db import models
 class Profile(models.Model):
     name = models.CharField(max_length=200)
     preference = models.CharField(max_length=200)
+
+class QuesModel(models.Model):
+    question = models.CharField(max_length=200,null=True)
+    op1 = models.CharField(max_length=200,null=True)
+    op2 = models.CharField(max_length=200,null=True)
+    op3 = models.CharField(max_length=200,null=True)
+    op4 = models.CharField(max_length=200,null=True)
+    ans = models.CharField(max_length=200,null=True)
+    
+    def __str__(self):
+        return self.question
