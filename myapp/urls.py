e6d1c5ee88b7b52d464fbbf191cd0dc45ed4ed4a
Alexmanix1 <alexandru.buruiana20@imperial.ac.uk>
2022-06-10 09:15:12 +0100

    feat: Link to another accepted page

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+<<<<<<< Updated upstream
+=======
+    path('accepted/', views.accepted),
+>>>>>>> Stashed changes
     path('', views.homepage),
 ]
