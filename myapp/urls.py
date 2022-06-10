58c2e84970338d6932d7004e7cd0e0aaeee1457f
Alexmanix1 <alexandru.buruiana20@imperial.ac.uk>
2022-06-10 09:27:29 +0100

    fix: I forgot to save changes :(

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,10 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('accepted/', views.accepted),
-=======
-    path(r'^accepted/', views.accepted),
->>>>>>> jiaqi-AcceptedScreen
     path('', views.homepage),
 ]
