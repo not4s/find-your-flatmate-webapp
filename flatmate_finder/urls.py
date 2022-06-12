e24fffc31bc3f74323aed0fedc4a82db3be5db7a
Jay Chung <jc2520@ic.ac.uk>
2022-06-12 09:41:44 +0900

    project name mysite -> flatmate_finder

--- flatmate_finder/urls.py
+++ flatmate_finder/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""flatmate_finder URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('home/', include('myapp.urls')),
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
