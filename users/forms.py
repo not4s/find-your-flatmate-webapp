6d8d5abd8a13dbe7e03a0efbb37d880c35d7ea9d
Jay Chung <jc2520@ic.ac.uk>
2022-06-17 15:02:46 +0900

    automatically trigger to create a profile after registration

--- users/forms.py
+++ users/forms.py
@@ -23,4 +23,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = ['image', 'about_me', 'search_location', 'studies_at']
--- users/views.py
+++ users/views.py
@@ -39,38 +39,3 @@ def profile(request):
     }
 
     return render(request, 'users/profile.html', context)
-
-# def profile_page(request):
-#     return HttpResponse('<h1>Profile Page Placeholder<h1>')
-
-# @login_required(login_url='signin')
-# def settings(request):
-
-#     user_profile = Profile.objects.get(user=request.user)
-
-#     if request.method == 'POST':
-
-#         if request.FILES.get == None:
-#             image = user_profile.profile_img
-#         else:
-#             image = request.FILES.get('image')
-
-#         firstname = request.POST['firstname']
-#         lastname = request.POST['lastname']
-#         email = request.POST['email']
-#         about = request.POST['about']
-#         location = request.POST['location']
-#         studies_at = request.POST['studies_at']
-
-#         user_profile.image = image
-#         user_profile.firstname = firstname
-#         user_profile.lastname = lastname
-#         user_profile.email = email
-#         user_profile.about = about
-#         user_profile.location = location
-#         user_profile.studies_at = studies_at
-#         user_profile.save()
-        
-#         return redirect('settings')
-
-#     return render(request, 'setting.html', {'user_profile': user_profile})
